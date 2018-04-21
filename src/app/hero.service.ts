4addd3e01f7d5880fe75f2feff1ab54a5c0d606a
Spring Wong <>
2018-04-21 15:41:51 +0800

    use rxjs for getHeroes function

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HERO } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class HeroService {
 
   constructor() { }
 
-  getHeroes() : Hero[] {
-  	return HEROES;
-  }
+  getHeroes(): Observable<Hero[]> {
+	  return of(HEROES);
+	}
 
 }
