e6f64e67804f63423361c8f07f2d0bcfe78806e7
Spring Wong <>
2018-04-21 16:27:41 +0800

    separate hero detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,20 +9,11 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-	hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes: Hero[];
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  selectedHero : Hero;
-  onSelect(hero : Hero) : void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
